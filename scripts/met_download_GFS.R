2c45da31f3cdb8286e91647bfa3559d1a572b546
crollinson <crollinson@gmail.com>
2020-04-13 16:58:12 -0500

    update met download script
    some file renaming; also add functionality to customized start/end date for extraction; also NEW MODELS!!!

---
+++ scripts/met_download_GFS.R
@@ -0,0 +1,13 @@
+
+# https://www.ncdc.noaa.gov/data-access/model-data/model-datasets/global-forcast-system-gfs
+# Example file directory: https://www.ncei.noaa.gov/thredds/catalog/model-gfs-004-files/202004/20200410/catalog.html
+# Example Query-builder URL: https://www.ncei.noaa.gov/thredds/ncss/model-gfs-004-files/202004/20200410/gfs_4_20200410_0600_384.grb2/dataset.html
+# Example built URL: https://www.ncei.noaa.gov/thredds/ncss/model-gfs-004-files/202004/20200410/gfs_4_20200410_0600_384.grb2?var=Precipitation_rate_surface&var=Temperature_surface&disableLLSubset=on&disableProjSubset=on&horizStride=1&time_start=2020-04-26T06%3A00%3A00Z&time_end=2020-04-26T06%3A00%3A00Z&timeStride=1&vertCoord=
+
+# File name structure: gfs_4_YYYYMMDD_HHHH_FFF.grb2; FFF = Forecast time point
+
+
+# Resolution options: 
+# -- 0.5 degrees to 192 hrs out (8 days)
+# -- 1.0 degrees to 384 hrs out (16 days)
+
