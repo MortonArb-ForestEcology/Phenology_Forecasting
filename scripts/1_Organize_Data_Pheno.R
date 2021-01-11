68cdae68f185f9bba22903115e0c28bf04726cde
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2021-01-11 10:19:23 -0600

    Updated using the newer scripts

--- scripts/1_Organize_Data_Pheno.R
+++ scripts/1_Organize_Data_Pheno.R
@@ -1,108 +1,59 @@
 #----------------------------------------------------------------------------------------------------------------------------------#
 # Script by : Lucien Fitzpatrick
-# Project: Living Collections Phenology Forecasting
-# Purpose: To use arb weather data and phenology monitoring data to create a predicitve model of bud burst timing
-#          This script serves as the initial data download, crosswalking, and orgnaizaiton needed for and the model input
-# Inputs: Old metstation data from 1895-2007 found in the "Arboretum Met Data/GHCN-Daily" google drive folder
-#         New metstation data from 2007-present found in the "Arboretum Met Data/GHCN-Daily" google drive folder
-#         Quercus 2018 to present phenology monitoring data from the googlesheet "Phenology_Observations_GoogleForm" in the "LivingCollections-Phenology/Data_Observations/" folder
-#         The clean_google_form.r script which defines the clean.google function. Found in the Github repository "Phenology_ LivingCollections"
-# Outputs:dat.comb dataframe that can be used in the Bayesian_GDD5-burst.r script and the Freqeuntist_GDD5-burst.Rin this repository
-# Notes: All script relating to met data is stolen from Christy ROllinson's script "02_MortonArb_Climate_BLoomTimes-1.r"
-#        The majority of the rest is currently a modification of that same script by Christy
-
+# Project: Spring Phenoogy Forecasting
+# Purpose: This script serves as the initial data download and organization of the arb data
+# Inputs: Quercus 2018 to present phenology monitoring data from the googlesheet "Phenology_Observations_GoogleForm" in the "LivingCollections-Phenology/Data_Observations/" folder
+#         The clean_google_form.r script which defines the clean.google function.
+#         The group_google_form.r script which defines the group.google function
+# Outputs: dat.burst dataframe that can be used for the model
+#          dat.leaf dataframe that can be used for the model
+# Notes:  Since the original data is kept by the authors, most users will not need to run this script. 
+#         The data frames created by this script are saved in this repository and can be used for the proceeding scripts.
 #-----------------------------------------------------------------------------------------------------------------------------------#
 
 #loading ggplot for visualization 
 library(ggplot2)
 
-path.hub <- "C:/Users/lucie/Documents/GitHub/"
-#path.hub <- "../.."
-
+path.hub <- "../"
 
 path.g <- "G:/My Drive"
-#path.g <- "/Volumes/GoogleDrive/My Drive"
+# path.g <- "/Volumes/GoogleDrive/My Drive"
+path <- "../data_processed/"
 dir.create("../data_processed/", recursive = T, showWarnings = F)
 
-#-------------------------------------------------#
-#This section is for downloaded the met data, pulling out data of interest, and calculating growing degree days
-#-------------------------------------------------#
-
-#Setting a shared file path for where the data are
-path.met <- file.path(path.g, "Arboretum Met Data/GHCN-Daily")
-
-# Read in the older dataset. This is because the GHCND at the arboretum changed in 2007 and we need to pull from both
-met.old <- read.csv(file.path(path.met, "MortonArb_GHCND-USC00119221_1895-2007.csv"))
-met.old$DATE <- as.Date(met.old$DATE) # Convert this column to a special datatype that's recognized as a date
-summary(met.old)
-
-# Read in the newer dataset
-met.new <- read.csv(file.path(path.met, "MortonArb_GHCND-USC00115097_2007-04-01_2019-12-31.csv"))
-met.new$DATE <- as.Date(met.new$DATE) # Convert this column to a special datatype that's recognized as a date
-summary(met.new)
-
-# Check to make sure we're in metric (earlier I had a mixture of units and that was bad)
-range(met.old$TMAX, na.rm=T)
-range(met.new$TMAX, na.rm=T)
-
-# Combine the old and the new datasets into a new data frame.  We don't want all columns, so just take the ones we care about
-met.all <- rbind(met.old[,c("STATION", "DATE", "PRCP", "SNOW", "SNWD", "TMAX", "TMIN")],
-                 met.new[,c("STATION", "DATE", "PRCP", "SNOW", "SNWD", "TMAX", "TMIN")])
-
-#These names help match daymet data so we can have one funciton to transform both
-#These temp names are bulkier the products use clear names regardless
-colnames(met.all) <- c("Station", "Date", "PRCP", "SNOW", "SNWD", "tmax..deg.c.", "tmin..deg.c.")
-
-
-#Pulling out useful date statistics. Only YDAY and YEAR are used in this function but others are added to the output data frame for future use
-met.all$year <- lubridate::year(met.all$Date)
-met.all$yday <- lubridate::yday(met.all$Date)
-
-y_start <- 1975
-y_end <- 2019
-
-#Subsetting to a useful range of years. Currently the max so doesn't do much
-met.all <- met.all[met.all$year>=y_start & met.all$year<=y_end,]
-
-
-#Reading in our function for calculating weather statistics of interest
-source(file.path(path.hub, "Phenology_Forecasting/scripts/weather_calc.R"))
-
-#Running our function to calculate weather statistics. Default year range is 1975-2019. Growing seaosn is yday 1 to 120
-met.all <- weather_calc(met.all)
-
-
-write.csv(met.all, "../data_processed/GHCN_met_all.csv", row.names=F)
 # -----------------------------
 # This section is to read in Phenology Monitoring data from our years of interest. THIS SECTION REQUIRES THE clean.google function
 # This function below takes in a vector of the genus of interest and a start and end year for the forms you want
 # -----------------------------
 
 #Calling in the clean.google function
-source(file.path(path.hub, "Phenology_LivingCollections/scripts/clean_google_form.R"))
+source(file.path(path.hub, "scripts/Clean_google_form.R"))
 
 #Calling in the group.google function. clean.google is needed for this.
-source(file.path(path.hub, "Phenology_Forecasting/scripts/Group_google.R"))
+source(file.path(path.hub, "scripts/Group_google.R"))
 
 #Enter the genus of interest as a vector and the final year of observations you want as a variable
 #The function will crash if you have a genus included without end years we have a form for i.e("Ulmus" with range 2018:2019)
 #This only matters for end years. Start years are adjusted to match the first year the genus has a form.
-Genus <- c("Quercus", "Acer")
+Genus <- c("Quercus")
 StartYear <- 2018
 EndYear <- 2019
 
-dat.pheno <- group.google(Genus, StartYear, EndYear)
+#Will produce message that look like errors that say 
+#"New names: Please choose the correct accession number for the tree you are observing.` -> `Please choose the correct accession number for the tree you are observing....5`"
+#But this is a normal sign that the function is working
+dat.oak <- group.google(Genus, StartYear, EndYear)
+
 #--------------------------------------------------------#
-# Here is where you pick out your species of interest
+# Here is where you  pull out phenometrics of interest (bud burst and leaf out)
 #--------------------------------------------------------#
-#Seperating out our chosen species
-dat.oak <- dat.pheno
 dat.oak$Date.Observed <- as.Date(dat.oak$Date.Observed)
-dat.oak$Bud <- as.factor(dat.oak$leaf.buds.observed)
+dat.oak$Bud <- as.factor(dat.oak$leaf.breaking.buds.observed)
+dat.oak$Leaf <- as.factor(dat.oak$leaf.present.observed)
 dat.oak <- dat.oak[!is.na(dat.oak$Date.Observed),]
 
 #pulling out bud burst information from out phenology data
-dat.oak <- subset(dat.oak, select = c("Date.Observed", "Year", "Species", "Bud", "PlantNumber"))
+dat.oak <- subset(dat.oak, select = c("Date.Observed", "Year", "Species", "Bud", "Leaf", "PlantNumber"))
 
 
 #Creating final frame containing the first burst for each year.
@@ -110,27 +61,31 @@ dat.burst <- aggregate(dat.oak[dat.oak$Bud=="Yes", "Date.Observed"],
                        by=dat.oak[dat.oak$Bud=="Yes", c("Species", "PlantNumber", "Year")], 
                        FUN=min)
 
+#Creating final frame containing the first burst for each year.
+dat.leaf <- aggregate(dat.oak[dat.oak$Leaf=="Yes", "Date.Observed"], 
+                      by=dat.oak[dat.oak$Leaf=="Yes", c("Species", "PlantNumber", "Year")], 
+                      FUN=min)
+
 #Adding in arboretum Lat and Long
 dat.burst$Latitude <- 41.8164
 dat.burst$Longitude <- -88.0549
+dat.burst$Site <- "Morton"
+
+#Adding in arboretum Lat and Long
+dat.leaf$Latitude <- 41.8164
+dat.leaf$Longitude <- -88.0549
+dat.leaf$Site <- "Morton"
 
 #Making easier names for the data frameYday",
-colnames(dat.burst) <- c("Species", "PlantNumber", "Year", "Date", "Latitude", "Longitude")
+colnames(dat.burst) <- c("Species", "PlantNumber", "Year", "Date", "Latitude", "Longitude", "Site")
 dat.burst$Yday <- lubridate::yday((dat.burst$Date))
 
+#Making easier names for the data frameYday",
+colnames(dat.leaf) <- c("Species", "PlantNumber", "Year", "Date", "Latitude", "Longitude", "Site")
+dat.leaf$Yday <- lubridate::yday((dat.leaf$Date))
 
-#Relic from when this combined this data with npn. keeping since it will return.
-dat.comb <- dat.burst
-
-dat.comb$Location <- paste(dat.comb$Latitude, dat.comb$Longitude, sep= " ")
-
-#Creating a new column in our phenology data frame that takes the date of earliest burst and gives us the cumulative gdd of that date from the met data
-dat.comb$GDD5.cum <- met.all$GDD5.cum[match(dat.comb$Date, met.all$Date)]
-dat.comb$GDD0.cum <- met.all$GDD0.cum[match(dat.comb$Date, met.all$Date)]
-dat.comb$NCD <- met.all$NCD[match(dat.comb$Date, met.all$Date)]
-dat.comb$GTmean <- met.all$GTmean[match(dat.comb$Date, met.all$Date)]
+#Creating raw data output
+write.csv(dat.burst, paste0(path, "/Oak_collection_budburst_raw.csv"), row.names=F)
+write.csv(dat.leaf, paste0(path, "/Oak_collection_leaf_raw.csv"), row.names=F)
 
 
-
-# Save dat.comb 
-write.csv(dat.comb, "../data_processed/Phenology_Met_combined.csv", row.names=F)
