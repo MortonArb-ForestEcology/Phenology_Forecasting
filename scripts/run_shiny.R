7b0c4c489ec99372f12c0c685a2911542ba2cb76
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2021-02-05 15:45:15 -0600

    Script to run our shiny app
    runApp is still for test runs

---
+++ scripts/run_shiny.R
@@ -0,0 +1,35 @@
+library(shiny)
+
+oaks <- googlesheets4::read_sheet("14rJUVhJ2pDSskyEzMM7KX5p3LTvpxiSnDoOw2Ngu2PY", sheet="QuercusCollection")
+oaks <- data.frame(oaks)
+
+dat.b <- read.csv("../data_processed/Oak_collection_budburst.csv")
+
+sp.catalogue <- as.data.frame(unique(dat.b$Species))
+colnames(sp.catalogue) <- c("Scientific")
+sp.catalogue$Common <- oaks$Common.Name[match(sp.catalogue$Scientific, oaks$Species)]
+
+sci <- as.data.frame(unique(dat.b$Species))
+colnames(sci) <- "Name"
+sci$Type <- "Scientific"
+
+com <- as.data.frame(sp.catalogue$Common)
+colnames(com) <- "Name"
+com$Type <- "Common"
+
+sp.index <- rbind(sci, com)
+
+write.csv(sp.catalogue, "shiny_app/Species_Name_Catalogue.csv", row.names = F)
+write.csv(sp.index, "shiny_app/Species_Name_Index.csv", row.names = F)
+
+runApp("shiny_app")
+
+setwd("../")
+setwd("shiny_app")
+
+rsconnect::deployApp(forceUpdate = T, launch.browser = F)
+
+stopApp(returnValue = invisible())
+
+
+
