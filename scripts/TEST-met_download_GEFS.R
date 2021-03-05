1e7a396d9e5b08fba4068faec7ac68b39f404051
Christy Rollinson <crollinson@mortonarb.org>
2021-03-05 15:47:53 -0600

    GEFS slight updates
    Note about the grid being real slow; exploratory aggregation of 16-day forecasts

--- scripts/TEST-met_download_GEFS.R
+++ scripts/TEST-met_download_GEFS.R
@@ -31,6 +31,7 @@ noaaGEFSpoint::noaa_gefs_download_downscale(site_list = neon_sites$site_id, lat_
 # -----------------------
 # Try syntax from eco4cast challenge: https://github.com/eco4cast/neon4cast-noaa-download/blob/master/launch_download_downscale.R
 # -----------------------
+# This works.  It's a little slow & only goes out 16 days, but it works.
 noaaGEFSpoint::noaa_gefs_download_downscale(site_list = paste0(site.name, 2),
                                             lat_list = lat.in,
                                             lon_list= lon.in,
@@ -42,9 +43,44 @@ noaaGEFSpoint::noaa_gefs_download_downscale(site_list = paste0(site.name, 2),
                                             num_cores = 1,
                                             method = "point",
                                             overwrite = FALSE)
+
+# Loading all of the different ensembles
+ftoday <- dir(file.path(outdir, "NOAAGEFS_6hr", paste0(site.name, 2), Sys.Date(), "00"))
+
+ncT <- ncdf4::nc_open(file.path(outdir, "NOAAGEFS_6hr", paste0(site.name, 2), Sys.Date(), "00", ftoday[1]))
+summary(ncT$var)
+ncT$dim$time
+length(ncT$dim$time$vals)
+
+# Note: When I'm testing this, it gets wonky because of daylight savings. :face_palm:
+df.6hr <- data.frame(Timestamp=seq.POSIXt(as.POSIXct(paste(Sys.Date(), "00:00")), by="6 hour", length.out=65), ens=rep(1:length(ftoday), each=65), tair=NA, prcp=NA)
+df.6hr$Date <- as.Date(substr(df.6hr$Timestamp, 1, 10))
+df.6hr[1:12,]
+
+for(i in 1:length(ftoday)){
+  row.ind <- which(df.6hr$ens==i)
+  
+  ncT <- ncdf4::nc_open(file.path(outdir, "NOAAGEFS_6hr", paste0(site.name, 2), Sys.Date(), "00", ftoday[i]))
+  df.6hr$tair[row.ind] <- ncdf4::ncvar_get(ncT, "air_temperature")
+  df.6hr$prcp[row.ind] <- ncdf4::ncvar_get(ncT, "precipitation_flux")
+  
+  ncdf4::nc_close(ncT)
+}
+# the first value for all precip is NA, so let just make it 0 for our snaity
+df.6hr$prcp[is.na(df.6hr$prcp)] <- 0
+summary(df.6hr)
+
+met.day <- aggregate(cbind(tair, prcp) ~ Date + ens, data=df.6hr, FUN=mean)
+met.day$prcp.day <- met.day$prcp*60*60*24/4 # should convert to mm/day
+summary(met.day)
+
+ggplot(data=met.day) +
+  geom_line(aes(x=Date, y=(tair-273.15)*9/5+32, group=ens))
 # -----------------------
 
-noaaGEFSpoint::noaa_gefs_download_downscale(site_list = paste0(site.name, 2),
+
+# This is SUPER slow because it's downloading ALL the data first BUT it would give us forecast out to 35 days in the future
+noaaGEFSpoint::noaa_gefs_download_downscale(site_list = paste0(site.name, "grid"),
                                             lat_list = lat.in,
                                             lon_list= lon.in,
                                             output_directory = outdir,
