0c834b67a36e08398b8749cf000ce7ab9e733498
Christy Rollinson <crollinson@mortonarb.org>
2021-03-05 16:01:15 -0600

    cleanup: delete broken attempts

--- scripts/TEST-met_download_GEFS.R
+++ scripts/TEST-met_download_GEFS.R
@@ -12,25 +12,7 @@ site.name="MortonArb"
 lat.in=41.812739
 lon.in=-88.072749
 
-noaaGEFSpoint::noaa_gefs_download_downscale(site_list = site.name, lat_list = lat.in, lon_list = lon.in, output_directory=outdir, forecast_time = "all", forecast_date = "all", downscale = F, run_parallel = FALSE, num_cores = 1, overwrite = FALSE)
-# -----------------------
-
-# -----------------------
-# Try Quinn's example
-# -----------------------
-site_file <- system.file("extdata", "noaa_download_site_list.csv", package = "noaaGEFSpoint")
-
-neon_sites <- read_csv(site_file)
-
-
-noaaGEFSpoint::noaa_gefs_download_downscale(site_list = neon_sites$site_id, lat_list = neon_sites$latitude, lon_list = neon_sites$longitude, output_directory=outdir, forecast_time = "all", forecast_date = "all", downscale = TRUE, run_parallel = FALSE, num_cores = 1, overwrite = FALSE)
-# -----------------------
-
-
-
-# -----------------------
 # Try syntax from eco4cast challenge: https://github.com/eco4cast/neon4cast-noaa-download/blob/master/launch_download_downscale.R
-# -----------------------
 # This works.  It's a little slow & only goes out 16 days, but it works.
 noaaGEFSpoint::noaa_gefs_download_downscale(site_list = paste0(site.name, 3),
                                             lat_list = lat.in,
