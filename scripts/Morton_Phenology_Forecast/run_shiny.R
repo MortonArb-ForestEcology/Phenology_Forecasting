c5f36ffa6ae21e48c0ea08d746d32f46a01ba6b6
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2021-03-10 12:04:42 -0600

    Fully notated script for running the shiny app

--- scripts/Morton_Phenology_Forecast/run_shiny.R
+++ scripts/Morton_Phenology_Forecast/run_shiny.R
@@ -1,40 +1,31 @@
+#----------------------------------------------------------------------------------------------------------------------------------#
+# Script by : Lucien Fitzpatrick
+# Project: Phenology forecasting app
+# Purpose: This script serves to launch the app locally or online
+# Inputs: 
+# Outputs: Runs the app
+# Notes: You can run the app locally or online. Which working directory you need to be in depends on which you are doing
+# You must be in the "shiny_app" folder to run online. You must be in this scripts wd to run it locally
+#-----------------------------------------------------------------------------------------------------------------------------------#
 library(shiny)
 library(dplyr)
 library(shiny)
 
-#setwd("../")
-#runApp("shiny_app")
+#This is to run the app LOCALLY
+#If you open this script and are in it's directory, all you have to do is runApp.
+#setwd("../")  #This is kept here for when I am bouncing between online and local runs
+runApp("shiny_app")
 
 
+#This section is to run the app ONLINE
 setwd("shiny_app")
 
 rsconnect::deployApp(forceUpdate = T, launch.browser = F)
 
+
+#This is how you manually stop the ONLINE app
 stopApp(returnValue = invisible())
 
 
-#Run this section if you need to update the phenology data
-#may be unneccessary as we use the sql
-oaks <- googlesheets4::read_sheet("14rJUVhJ2pDSskyEzMM7KX5p3LTvpxiSnDoOw2Ngu2PY", sheet="QuercusCollection")
-oaks <- data.frame(oaks)
-
-dat.b <- read.csv("../data_processed/Oak_collection_budburst.csv")
-
-sp.catalogue <- as.data.frame(unique(dat.b$Species))
-colnames(sp.catalogue) <- c("Scientific")
-sp.catalogue$Common <- oaks$Common.Name[match(sp.catalogue$Scientific, oaks$Species)]
-
-sci <- as.data.frame(unique(dat.b$Species))
-colnames(sci) <- "Name"
-sci$Type <- "Scientific"
-
-com <- as.data.frame(sp.catalogue$Common)
-colnames(com) <- "Name"
-com$Type <- "Common"
-
-sp.index <- rbind(sci, com)
-
-write.csv(sp.catalogue, "shiny_app/Species_Name_Catalogue.csv", row.names = F)
-write.csv(sp.index, "shiny_app/Species_Name_Index.csv", row.names = F)
 
 
