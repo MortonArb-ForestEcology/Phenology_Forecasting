1503947796a41d89ae00e5f5b1b3d9a1a4d5fe9d
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2021-03-10 12:26:05 -0600

    Updated notation and file paths on ui

--- scripts/Morton_Phenology_Forecast/shiny_app/ui.R
+++ scripts/Morton_Phenology_Forecast/shiny_app/ui.R
@@ -1,3 +1,13 @@
+#----------------------------------------------------------------------------------------------------------------------------------#
+# Script by : Lucien Fitzpatrick
+# Project: Phenology forecasting app
+# Purpose: This script is the ui side of the shiny app
+# Inputs: Weather_ArbCOOP_historical_latest.csv created by M1_Meterology_download.R
+#         Weather_Arb_forecast_ensemble_latest.csv created by M1_Meterology_download.R
+#         Species_Name_Catalogue.csv created by SP_Names.R
+# Outputs: 
+# Notes: The first half of this script is taken from M2_Meteorology_Graphing_Bayes.R
+#-----------------------------------------------------------------------------------------------------------------------------------#
 library(shiny)
 library(ggplot2)
 library(plotly)
@@ -11,19 +21,22 @@ path.in <- "data_raw/meteorology"
 # ---------------
 # Met Data
 # ---------------
-dat.ghcn <- read.csv(file.path(path.in, "data", "Weather_ArbCOOP_historical_latest.csv"))
+dat.ghcn <- read.csv(file.path(path.in, "Weather_ArbCOOP_historical_latest.csv"))
 dat.ghcn$DATE <- as.Date(dat.ghcn$DATE)
 
 
-dat.forecast <- read.csv(file.path(path.in, "data", "Weather_Arb_forecast_ensemble_latest.csv"))
+dat.forecast <- read.csv(file.path(path.in, "Weather_Arb_forecast_ensemble_latest.csv"))
 dat.forecast$DATE <- as.Date(dat.forecast$DATE)
 
-dat.b <- read.csv("data_processed/Oak_collection_budburst.csv")
-sp.list <- sort(unique(dat.b$Species))
+#Species names for the name pickers
+sp.catalogue <- read.csv("Species_Name_Catalogue.csv")
+
+sp.list <- sort(unique(sp.catalogue$Scientific))
 name.type <- c("Scientific", "Common")
 
 fluidPage(
   
+  #Allowing the choice between scientific and common
   selectInput("Convention", "Choose a naming style:", list(Convention=as.list(name.type))),
   uiOutput("select_Species"),
 
