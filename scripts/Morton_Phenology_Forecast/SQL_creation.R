c5a3f13a443dc9cb9ac260b89eb6a32b02fc8c8b
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2021-03-10 12:04:15 -0600

    Moved the location to fit the workflow

--- scripts/Morton_Phenology_Forecast/SQL_creation.R
+++ scripts/Morton_Phenology_Forecast/SQL_creation.R
@@ -4,18 +4,18 @@
 library(RSQLite)
 
 #Reading in budburst model
-bud.files <- list.files(path = "Morton_Phenology_Forecast/data_processed/model_output/", pattern = "TT_model_budburst.csv", full.names = T)
+bud.files <- list.files(path = "../../data_processed/model_output/", pattern = "TT_model_budburst.csv", full.names = T)
 Budburst_Model <- as.data.frame(sapply(bud.files, read.csv, simplify=FALSE) %>% 
                                   bind_rows(.id = "id"))
 
 Budburst_Model <- Budburst_Model[,c("THRESH", "aPrec", "sd", "species")]
 
 #Reading in the oak observations
-dat.b <- read.csv("../data_processed/Oak_collection_budburst.csv")
+dat.b <- read.csv("../../data_processed/Oak_collection_budburst.csv")
 
 
 #This creates the database and "conn" becomes the variable noting the connection path
-conn <- dbConnect(RSQLite::SQLite(), "Morton_Phenology_Forecast/Arb_Pheno.db")
+conn <- dbConnect(RSQLite::SQLite(), "shiny_app/Arb_Pheno.db")
 dbWriteTable(conn, "Budburst_Model", Budburst_Model)
 dbWriteTable(conn, "Budburst_Obs", dat.b)
 
