76c217476099cb5048b494d4203c9fbbd53452fc
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-02-02 15:38:43 -0600

    A new script that sources the workflow so only this script needs to be run automatically daily

---
+++ scripts/Morton_Phenology_Forecast/Full_workflow.R
@@ -0,0 +1,24 @@
+#----------------------------------------------------------------------------------------------------------------------------------#
+# Script by : Lucien Fitzpatrick
+# Project: Phenology forecasting app
+# Purpose: This script serves to run throught our daily workflow
+# Inputs: 
+# Outputs: Downloads the new weather, updates the forecasts, and launches the app online
+# Notes: This just sources the other scripts in this directory
+#-----------------------------------------------------------------------------------------------------------------------------------#
+library(shiny)
+library(ggplot2)
+library(plotly)
+library(stringr)
+library(shinyWidgets)
+library(dplyr)
+library(gridExtra)
+
+#Downloading the new weatehr data
+source("1_M1_Meterology_download.R")
+
+#Uploading the new data to the shiny app's folder
+source("2_SQL_creation.R")
+
+#Launching the app to the internet
+source("3_run_shiny.R")
