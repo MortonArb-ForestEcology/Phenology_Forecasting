cb704af1e3f503351f323e6020bd50d7227222e5
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-02-11 16:56:35 -0600

    Removing old visualizaiton
    Removed the old visualization that is now back in the server. I also changed some file paths because of incongruence

--- scripts/LIVE-Morton_Phenology_Forecast/LIVE-2_data_org.R
+++ scripts/LIVE-Morton_Phenology_Forecast/LIVE-2_data_org.R
@@ -24,8 +24,8 @@ if(!dir.exists(path.temp)) dir.create(path.temp, recursive=T, showWarnings = F)
 path.vis <- "MortonArb_PhenoForecast/figures/"
 if(!dir.exists(path.vis)) dir.create(path.vis, recursive=T, showWarnings = F)
 
-path.date <- file.path("MortonArb_PhenoForecast/figures", Sys.Date())
-if(!dir.exists(path.date)) dir.create(path.date, recursive=T, showWarnings = F)
+path.burst <- file.path("MortonArb_PhenoForecast/data/budburst")
+if(!dir.exists(path.burst)) dir.create(path.burst, recursive=T, showWarnings = F)
 
 #Reading in budburst model
 path.mod <- "../../data_processed/model_output"
@@ -90,48 +90,36 @@ fc.sp$VAR <- gsub("\\..*","",(row.names(fc.sp)))
 rownames(fc.sp) <- NULL
 colnames(fc.sp) <- c("PRED.DATE", "YDAY", "TYPE", "mean", "min", "max", "VAR")
  
-write.csv(fc.sp, file.path(path.temp, paste0("Forecast_data_", Sys.Date(),".csv")), row.names = F)
-
-# -------------------------------------
-# Creating our budburst visualizations
-# -------------------------------------
-# Creating some day and axis labels
-day.labels <- data.frame(Date=seq.Date(as.Date("2021-01-01"), as.Date("2021-12-31"), by="month"))
-day.labels$yday <- lubridate::yday(day.labels$Date)
-day.labels$Text <- paste(lubridate::month(day.labels$Date, label=T), lubridate::day(day.labels$Date))
-summary(day.labels)
+write.csv(fc.sp, file.path(path.temp, "meteorology", paste0("Forecast_data_", Sys.Date(),".csv")), row.names = F)
 
 calc.bud <- function(dat, VAR, THRESH){
   min(dat[which(dat[,VAR] >= THRESH),"YDAY"])
 }
 
-day.labels2 <- data.frame(Date=seq.Date(as.Date("2021-01-03"), as.Date("2021-7-01"), by="day"))
-day.labels2$yday <- lubridate::yday(day.labels2$Date)
-day.labels2$Text <- paste(lubridate::month(day.labels2$Date, label=T), lubridate::day(day.labels2$Date))
-summary(day.labels2)
-
+#Need to optomize this later
+#pred.sp <- data.frame(matrix(ncol = 2, nrow = length(unique(b.model$species))*100))
 pred.sp <- data.frame()
-lim.comb <- data.frame()
+
+lim.comb <- data.frame(matrix(ncol = 6, nrow = length(unique(b.model$species))))
+colnames(lim.comb) <- c("Species", "lb", "ub", "mean", "min", "max")
+
+
+count <- 1
 for(SP in unique(b.model$species)){
 #Pulling out the gdd5.cum vlaues
+
   thresh <- b.model[b.model$species == SP, "THRESH"]
-  
-  #Taking the mean date of each year
-  #Year <- unique(dat.b[dat.b$Species == SP, "Year"])
-  #prev.date <- as.data.frame(Year)
-  #for(YR in unique(dat.b[dat.b$Species == SP, "Year"])){
-  #  prev.date[prev.date$Year == YR, "Mean.Date"] <- mean.Date(as.Date(format(as.Date(dat.b[dat.b$Species == SP & dat.b$Year == YR,"Date"]),"%m-%d"), format = "%m-%d"))
-  #}
-  
+
   pred.array <- array(dim=c(length(thresh), length(unique(dat.forecast$ENS))))
   #We want to pull from the Budburst_Model table for out GDD predictions
   ens <- unique(dat.forecast$ENS)
   for(i in 1:length(ens)){
     pred.array[,i] <- unlist(lapply(dat=dat.forecast[dat.forecast$ENS==ens[i],], FUN=calc.bud, VAR="GDD5.cum", thresh))
   }
-  pred.df <- data.frame(yday=sample(as.vector(pred.array), 50))
+  pred.df <- data.frame(yday=sample(as.vector(pred.array), 100))
   pred.df$Species <- SP
   
+  pred.sp[, count:count+100]
   pred.sp <- rbind(pred.sp, pred.df)
   
   quant <- quantile(pred.array, c(0.125, 0.875))
@@ -142,92 +130,12 @@ for(SP in unique(b.model$species)){
   colnames(dat.lim) <- c("Species", "lb", "ub", "mean", "min", "max")
   row.names(dat.lim) <- NULL
   
-  lim.comb <- rbind(lim.comb, dat.lim)
+  lim.comb[count, ] <- dat.lim
+  count <- count +1
 }
 
 write.csv(pred.sp, file.path(path.burst, paste0("Oak_Budburst_Prediction_", Sys.Date() ,".csv")), row.names = F)
 write.csv(lim.comb, file.path(path.burst, paste0("Oak_Prediciton_Summary_", Sys.Date() ,".csv")), row.names = F)
-
-
-  plot.threshB <- ggplot(data=dat.ghcn) +
-    stat_summary(data=dat.ghcn, aes(x=YDAY, y=threshB), fun=mean, color="black", geom="line", size=1, na.rm = T) +
-    geom_line(data=dat.ghcn[dat.ghcn$YEAR==lubridate::year(Sys.Date()), ], aes(x=YDAY, y=threshB, color="observed"), size=2) +
-    geom_ribbon(data=ens.forecast$threshB[ens.forecast$threshB$TYPE=="forecast",], aes(x=YDAY, ymin=min, ymax=max, fill="forecast"), alpha=0.5) +
-    geom_line(data=ens.forecast$threshB[ens.forecast$threshB$TYPE=="forecast",], aes(x=YDAY, y=mean, color="forecast")) +
-    scale_color_manual(name="data type", values = c("skyblue", "blue2")) +
-    scale_fill_manual(name="data type", values = c("skyblue", "blue2")) +
-    theme_bw() +
-    guides(fill="none") +
-    ggtitle(paste0("Cumulative GDD5 across time for ", SP))+
-    theme(legend.position = c(0.2, 0.75),
-          legend.title=element_blank(),
-          legend.background = element_blank(),
-          panel.grid.minor.x = element_blank(),
-          panel.grid.minor.y = element_blank())
-  
-  if(Sys.Date()<=as.Date(paste0(lubridate::year(Sys.Date()), "-06-20"))) {
-    plot.threshB <- plot.threshB + 
-      scale_y_continuous(name="Cum. GrowDD, base 5 C" ,expand=c(0,0)) +
-      scale_x_continuous(name="Day of Year", expand=c(0,0), limits=c(1, 180), breaks=day.labels$yday[seq(2, 12, by=1)], labels=day.labels$Text[seq(2, 12, by=1)])  +
-      coord_cartesian(ylim=c(0, max(dat.ghcn$threshB[dat.ghcn$YDAY<=180])))
-    
-    # pheno.sum; dat.thresh
-  } else {
-    plot.threshB <- plot.threshB + 
-      scale_y_continuous(name="Cum. ChillDD, base -2 C" ,expand=c(0,0)) +
-      scale_x_continuous(name="Day of Year", expand=c(0,0), limits=c(155, 366), breaks=day.labels$yday[seq(2, 12, by=1)], labels=day.labels$Text[seq(2, 12, by=1)])   +
-      coord_cartesian(ylim=c(0, max(dat.ghcn$threshB[dat.ghcn$YDAY>=155])))
-  }  
-  
-  Mean.d.temp <- ggplot(data=dat.ghcn) +
-    stat_summary(data=dat.ghcn, aes(x=YDAY, y=TMEAN), fun=mean, color="black", geom="line", size=1,  na.rm = T) +
-    geom_line(data=dat.ghcn[dat.ghcn$YEAR==lubridate::year(Sys.Date()), ], aes(x=YDAY, y=TMEAN, color="observed"), size=2) +
-    geom_ribbon(data=ens.forecast$TMEAN[ens.forecast$TMEAN$TYPE=="forecast",], aes(x=YDAY, ymin=min, ymax=max, fill="forecast"), alpha=0.5) +
-    geom_line(data=ens.forecast$TMEAN[ens.forecast$TMEAN$TYPE=="forecast",], aes(x=YDAY, y=mean, color="forecast"), na.rm = T) +
-    scale_color_manual(name="data type", values = c("skyblue", "blue2")) +
-    scale_fill_manual(name="data type", values = c("skyblue", "blue2")) +
-    scale_x_continuous(name="Day of Year", expand=c(0,0), breaks=day.labels$yday[seq(2, 12, by=1)], labels=day.labels$Text[seq(2, 12, by=1)], limits = c(0,180))  +
-    scale_y_continuous(name="Mean Daily Temp (C)" ,expand=c(0,0)) +
-    theme_bw() +
-    guides(fill="none") +
-    ggtitle(paste0("Mean temp (C) across time for ",SP))+
-    theme(legend.position = c(0.2, 0.75),
-          legend.title=element_blank(),
-          legend.background = element_blank(),
-          panel.grid.minor.x = element_blank(),
-          panel.grid.minor.y = element_blank()) +
-    geom_rect(data=dat.lim["q75",],
-              aes(xmin = lb, xmax=ub, ymin=-Inf, ymax=Inf), fill="green4", alpha=0.5)+
-    geom_vline(data = prev.date, aes(xintercept=lubridate::yday(Mean.Date), linetype = as.character(Year)))+
-    theme(text = element_text(size = 15))     
-  
-  Cum.gdd5 <- plot.threshB + 
-    geom_rect(data=dat.lim["q75",],
-              aes(xmin = lb, xmax=ub, ymin=-Inf, ymax=Inf), fill="green4", alpha=0.5)+
-    geom_vline(data = prev.date, aes(xintercept=lubridate::yday(Mean.Date), linetype = as.character(Year)))+
-    theme(text = element_text(size = 15))     
-  
-  Prob.dist <- ggplot() + 
-    geom_density(data=pred.df, aes(x=x), adjust=3.5, fill="green3", alpha=0.5) +
-    geom_vline(data=dat.lim["q75",], aes(xintercept=lb), color="darkgreen", linetype="dashed") +
-    geom_vline(data=dat.lim["q75",], aes(xintercept=ub), color="darkgreen", linetype="dashed") +
-    scale_x_continuous(name="Day of Year", expand=c(0,0), breaks=day.labels2$yday[seq(8, nrow(day.labels2), by=7)], labels=day.labels2$Text[seq(8, nrow(day.labels2), by=7)])  +
-    scale_y_continuous(name="Probability of Bud Burst" ,expand=c(0,0)) +
-    theme_bw() +
-    guides(fill="none") +
-    ggtitle(paste0("75% Confidence Interval for ",SP))+
-    theme(legend.position = c(0.5, 0.2),
-          legend.title=element_blank(),
-          legend.background = element_blank(),
-          panel.grid.minor.x = element_blank(),
-          panel.grid.minor.y = element_blank())+
-    theme(text = element_text(size = 15))     
-  
-
-  plots <- plot_grid(Mean.d.temp, Cum.gdd5, Prob.dist, nrow = 1)
-  save(plots, file = paste0(path.date, "/", SP, "_visualization.rdata"))
-
-
 
 #Creating the name indexes used for the name picker (This is for having both common and scientific names)
 ## This is a file Lucien created based on something in our Google Drive and will need to be manually updated if necessary; This is is a small, static file, so now pushed to GitHub so everybody can have the same version
@@ -256,5 +164,7 @@ colnames(past.dates) <- c("Date")
 write.csv(sp.index , file.path(path.temp, "Species_Index.csv"), row.names = F)
 write.csv(sp.catalogue, file.path(path.temp, "Species_Catalogue.csv"), row.names = F)
 write.csv(past.dates, file.path(path.temp, "Old_Forecast_List.csv"), row.names = F)
+write.csv(dat.ghcn, file.path(path.temp, "meteorology", "Historical_Weather.csv"), row.names = F)
+
 
 print("Data Organizaiton & Prediction Workflow Complete!")
