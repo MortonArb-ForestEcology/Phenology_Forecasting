b039c6c22e7cc686acd0d197770ef2914d2e6110
Christy Rollinson <crollinson@gmail.com>
2022-02-12 09:39:26 -0600

    typo corrections

--- scripts/LIVE-Morton_Phenology_Forecast/LIVE-00_Full_workflow.R
+++ scripts/LIVE-Morton_Phenology_Forecast/LIVE-00_Full_workflow.R
@@ -6,15 +6,15 @@
 # Outputs: Downloads the new weather, updates the forecasts, and launches the app online
 # Notes: This just sources the other scripts in this directory
 #-----------------------------------------------------------------------------------------------------------------------------------#
-library(shiny)
-library(ggplot2)
-library(plotly)
-library(stringr)
-library(shinyWidgets)
-library(dplyr)
-library(gridExtra)
+# library(shiny)
+# library(ggplot2)
+# library(plotly)
+# library(stringr)
+# library(shinyWidgets)
+# library(dplyr)
+# library(gridExtra)
 
-#Downloading the new weatehr data
+#Downloading the new weather data
 source("LIVE-1_M1_Meterology_download.R")
 
 #Uploading the new data to the shiny app's folder
