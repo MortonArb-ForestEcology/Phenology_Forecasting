4958ab1a2b73963ac776d5783c6e99b9a7d00d13
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-02-15 15:28:08 -0600

    Adding our explanation text to the top and a title

--- scripts/DEVELOP-Morton_Phenology_Forecast/shiny_app/ui.R
+++ scripts/DEVELOP-Morton_Phenology_Forecast/shiny_app/ui.R
@@ -29,8 +29,16 @@ fc.df <- read.csv(file.path(path.in, "Old_Forecast_List.csv"))
 #This puts everything on a single page where I can specify the organizaiton easily
 fluidPage(
   
+  titlePanel("Morton Arboretum Spring Budburst Forecast"),
+  
   #This defines what will be in the first row of the page
-  #Currently that is the naming convention selection and previous forecast slider
+  #Currently that is our explanation of what the app is
+  fluidRow(
+    p("This app provides a visualization of the predicted dates of the budburst phenophase for", em("Quercus"), "(oak) species in the", em("Quercus"), 
+      "collection at The Morton Arboretum utilizing data collected from citizen science phenology monitors.")
+  ),
+  
+  #The second row is the naming convention selection and previous forecast slider
   fluidRow(
   #Allowing the choice between scientific and common: Maybe remove given the audience and functionality quirks
   column(width = 4, selectInput("Convention", "Choose a naming style:", list(Convention=as.list(name.type)))),
@@ -38,18 +46,18 @@ fluidPage(
   #Allowing for a slider between the forecasts
   column(width = 4, sliderTextInput("Forecast date", "Previous forecasts", choices=fc.df$Date, selected = as.character(max(fc.df$Date))))),
   
-  #Deciding what is in the second row
+  #Deciding what is in the third row
   fluidRow(
     #Picking the species you want to see
     column(width = 4, uiOutput("select_Species"))),
   
-  #Deciding what is in the third row
+  #Deciding what is in the fourth row
   fluidRow(
     
     #The submit button and warning text
     column(width = 4, "Click submit to see and update results"),
             column(width = 2, submitButton("Submit"))),
-  #Fourth row
+  #Fifth row
     fluidRow(
       #The graphs themselves. The only show up when the submit button is pressed
                   uiOutput("plot.thresh.ui")),
